cef22001127198cba9d2cc9642ccc3b7d17e8c05
Emmanuel Biose <slimbion@gmail.com>
2019-10-24 02:12:52 +0100

    Added Android permission for Geolocation, Created UI for GPS feature

--- App.js
+++ App.js
@@ -1,47 +1,88 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
 import React, {Component} from 'react';
-import {Alert, StyleSheet, Text, View, TouchableOpacity} from 'react-native';
-import Geolocation from 'react-native-geolocation-service';
+import {StyleSheet, Text, View} from 'react-native';
 
 class App extends Component {
-  state = {
-    location: null,
-  };
-
-  findCoordinates = () => {
-    Geolocation.getCurrentPosition(
-        position => {
-          const location = JSON.stringify(position);
-
-          this.setState({ location });
-        },
-        error => Alert.alert(error.message),
-        {enableHighAccuracy: true, timeout: 1500, maximumAge: 10000},
-    );
-  };
-
   render() {
     return (
-        <View style={styles.container}>
-          <TouchableOpacity onPress={this.findCoordinates}>
-            <Text style={styles.welcome}>Find My Coords?</Text>
-            <Text>Location: {this.state.location}</Text>
-          </TouchableOpacity>
+      <View style={styles.root}>
+        <View style={styles.coordinatesContainer}>
+          <Text style={styles.coordinatesTextTitle}>Longitude</Text>
+          <Text style={styles.coordinatesTextValue}>20</Text>
         </View>
+
+        <View style={styles.coordinatesContainer}>
+          <Text style={styles.coordinatesTextTitle}>Latitude</Text>
+          <Text style={styles.coordinatesTextValue}>40</Text>
+        </View>
+
+        <View style={styles.informationContainer}>
+          <Text>This is a dummy text for the GPS function . . .</Text>
+        </View>
+
+        <View style={styles.button}>
+          <Text style={styles.buttonText}>Turn on GPS</Text>
+        </View>
+      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+  root: {
+    padding: 10,
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
+  coordinatesContainer: {
+    alignItems: 'center',
+    backgroundColor: '#349beb',
+    borderRadius: 5,
+    elevation: 10,
+    flexDirection: 'row',
+    height: 120,
+    justifyContent: 'space-between',
     margin: 10,
+    padding: 20,
+  },
+  coordinatesTextTitle: {
+    color: '#FFFFFF',
+    fontSize: 25,
+    fontWeight: 'bold',
+  },
+  coordinatesTextValue: {
+    color: '#FFFFFF',
+    fontSize: 25,
+  },
+  informationContainer: {
+    backgroundColor: '#f5f4f2',
+    height: 120,
+    borderColor: '#000000',
+    borderWidth: 1,
+    borderRadius: 5,
+    margin: 10,
+    padding: 20,
+  },
+  button: {
+    backgroundColor: '#349beb',
+    borderRadius: 5,
+    elevation: 10,
+    height: 50,
+    padding: 20,
+    margin: 10,
+    width: 150,
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonText: {
+    color: '#FFFFFF',
+    fontWeight: 'bold',
   },
 });
 
